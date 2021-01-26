0323efa9a0e99df3ed6675f76d6123b9fa95cb78
Kanchan Joshi <30471843+koderjoker@users.noreply.github.com>
2021-01-26 13:09:44 +0530

    Add functionality for searching in website
    Closes #3

--- src/components/threedcarmodel.component.js
+++ src/components/threedcarmodel.component.js
@@ -1,1 +1,32 @@
-// Code
+import React, {useState, useEffect, useRef, useMemo} from 'react'
+import ReactDOM from 'react-dom'
+import "../CSS/threedcarmodel.css"
+
+const ShowCar = props => {
+    function filterFunction(){
+        let input = document.getElementById("filterCarSelectList")
+        let filter = input.value.toUpperCase()
+        let li = document.getElementsByTagName("li")
+
+        for(let itr=0; itr < li.length; itr++){
+            let text = li[itr].textContent || li[itr].innerText
+
+            if( text.toUpperCase().indexOf(filter) !== -1 ){
+                li[itr].style.display = ""
+            } 
+            else{
+                li[itr].style.display = "none"
+            }
+        }
+    }
+    
+    // Implement setNewCarItem function here
+  
+    const carListItems = carList.map((carItem)=>{
+        return (
+            <li key={carItem} onClick={setNewCarItem}>
+                {carItem}
+            </li>
+        )
+    })
+}
